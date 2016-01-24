23dcebff45cd542aacf2003a152cfb4e1e7fa668
Megan Smith <megan@tokaypress.com>
2016-01-24 10:51:28 -0800

    Complete filter, reject, and uniq

--- src/underbar.js
+++ src/underbar.js
@@ -77,16 +77,33 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var filtered = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        filtered.push(item);
+      }
+    });
+    return filtered;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item) {
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniqArr = [];
+    _.each(array, function(item) {
+      if (_.indexOf(uniqArr, item) === -1) {
+        uniqArr.push(item);
+      }
+    });
+    return uniqArr;
   };
 
 
